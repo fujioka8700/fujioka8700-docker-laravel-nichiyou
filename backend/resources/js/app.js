26860c9a7b5aaed0f45cf1e78b8f2d8c472e3b9b
fujioka8700 <fujioka8700@gmail.com>
2021-10-13 15:39:27 +0900

    [fix] bootstrap削除

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,3 +1,7 @@
+window._ = require('lodash');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
