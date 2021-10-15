165408864bc4ced18419cbc9f530aebdbb5585b5
fujioka8700 <fujioka8700@gmail.com>
2021-10-15 12:38:01 +0900

    [add] BreakPointを表示

---
+++ backend/resources/views/top.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="著者情報">
+    <meta name="description" content="検索結果の説明文">
+    <meta name="keywords" content="カンマ区切りで、キーワード">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>日用品管理アプリ</title>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+</head>
+<body>
+    <div id="app">
+        <tailwindcss-breakpoint></tailwindcss-breakpoint>
+    </div>
+
+</body>
+</html>
