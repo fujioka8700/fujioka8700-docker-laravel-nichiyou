6c62ae576ed2bdc63fa9c076bf44dd7cd01beab5
fujioka8700 <fujioka8700@gmail.com>
2021-10-15 14:31:11 +0900

    [fix] sass関連削除

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,7 +1,3 @@
-window._ = require('lodash');
-window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 import Vue from 'vue'
 import TailwindcssBreakpoint from './components/TailwindCssBreakpoint.vue'
 
